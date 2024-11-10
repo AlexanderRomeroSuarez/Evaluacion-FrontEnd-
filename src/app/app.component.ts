f080b3d6a8e80032f2924e36140cca30450c5244
Alex Romero <xander_tubb@hotmail.com>
2024-11-10 18:16:26 -0500

    se modificó el título en el app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Evaluacion--FronEnd';
+  title = 'ejemplo de FronEnd';
 }
