64fa7a2c99c96581f1738eb4591ab98e7071d29d
Alex Romero <xander_tubb@hotmail.com>
2024-11-11 20:13:09 -0500

    se fusiono con cherry.pick

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+<<<<<<< HEAD
   title = 'EvaluacionFronEnd';
+=======
+  title = 'ejemplo de operadores';
+
+  suma(a, b){
+   return a + b ;
+  }
+
+  resta(a, b){
+    return a - b ;
+   }
+
+   division(a, b){
+    return a / b ;
+   }
+
+   multiplicar(a, b){
+    return a * b ;
+   }
+>>>>>>> e146c5a (se corrigio el tutke)
 }
