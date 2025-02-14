3f20a5e1a92437db35cc52c6f8cd28e30dca7661
Alex Romero <xander_tubb@hotmail.com>
2025-02-14 12:44:09 -0500

    se modificó el mensaje con el comando reword, agregar clientes

---
+++ src/app/Clientes/add-cliente/add-cliente.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-cliente',
+  imports: [],
+  templateUrl: './add-cliente.component.html',
+  styleUrl: './add-cliente.component.scss'
+})
+export class AddClienteComponent {
+ constructor(){
+  
+ }
+}
