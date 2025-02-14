851309e07505d0e9dad5b49139b43e7afd6a8d75
Alex Romero <xander_tubb@hotmail.com>
2025-02-14 12:44:09 -0500

    nuevo componente para agregar clientes

---
+++ src/app/Clientes/add-cliente/add-cliente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-cliente',
+  imports: [],
+  templateUrl: './add-cliente.component.html',
+  styleUrl: './add-cliente.component.scss'
+})
+export class AddClienteComponent {
+
+}
